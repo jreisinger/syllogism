09bf46d2112b579b8d445399242f45524130ae66
Jozef Reisinger <jozef.reisinger@gmail.com>
2022-04-18 16:50:49 +0200

    start from beginning

--- syllogism.go
+++ syllogism.go
@@ -2,76 +2,29 @@
 // Syllogism is a part of Aristotelian (or common sense) logic.
 package syllogism
 
-import (
-	"fmt"
-)
+import "fmt"
 
-const (
-	PropositionMajor PropositionType = iota // general principle
-	PropositionMinor                        // particular case
-)
-
-type PropositionType int32
-
-// Syllogism connects the subject and predicate terms in its two propositions.
-type Syllogism struct {
-	PropositionMajor Proposition
-	PropositionMinor Proposition
-	Middle           Term
-}
-
-// Proposition is a declarative sentence, affirmative or negative, that can be
-// either true or false.
+// Proposition is a declarative sentence.
 type Proposition struct {
-	Type      PropositionType
 	Subject   Term
-	Verb      string // e.g., be
-	Negative  bool   // e.g., not be
 	Predicate Term
-	False     bool
-}
-
-// Conclusion is the application of a major proposition to a minor proposition.
-type Conclusion struct {
-	Term     Term
-	Verb     string
-	Negative bool
 }
 
 // Term is the subject or predicate of a proposition expressing a concept.
 type Term []string
 
-// Conclude generates a conclusion.
-func (s Syllogism) Conclude() (Conclusion, error) {
-	if !eq(s.PropositionMajor.Subject, s.PropositionMinor.Predicate) {
-		return Conclusion{}, fmt.Errorf("Subject of major proposition (%v) doesn't match predicate of minor proposition (%v)", s.PropositionMajor.Subject, s.PropositionMinor.Predicate)
-	}
-	// if s.PropositionMajor.Verb != s.PropositionMinor.Verb {
-	// 	return Conclusion{}, fmt.Errorf("verbs (%v, %v) of propositions don't match", s.PropositionMajor.Verb, s.PropositionMinor.Verb)
-	// }
-
-	var conclusion Conclusion
-	conclusion.Verb = s.PropositionMajor.Verb
-
+// Conclude connects the subject and predicate terms in its two propositions.
+// Major proposition states a general principle, minor proposition brings a
+// particalur case under that priciple.
+func Conclude(major, minor Proposition) (Proposition, error) {
+	var conclusion Proposition
 	switch {
-	case !s.PropositionMajor.Negative && !s.PropositionMinor.Negative:
-		conclusion.Negative = false
-	case s.PropositionMajor.Negative && !s.PropositionMinor.Negative:
-		conclusion.Negative = true
-	case !s.PropositionMajor.Negative && s.PropositionMinor.Negative:
-		conclusion.Negative = true
-	case s.PropositionMajor.Negative && s.PropositionMinor.Negative:
-		conclusion.Negative = true
+	case eq(major.Subject, minor.Predicate):
+		conclusion.Subject = minor.Subject
+		conclusion.Predicate = major.Predicate
+	default:
+		return conclusion, fmt.Errorf("can't conclude from %v and %v", major, minor)
 	}
-
-	if s.PropositionMajor.False || s.PropositionMinor.False {
-		conclusion.Negative = !conclusion.Negative
-	}
-
-	var t Term
-	t = append(t, s.PropositionMinor.Subject...)
-	t = append(t, s.PropositionMajor.Predicate...)
-	conclusion.Term = append(conclusion.Term, t...)
 	return conclusion, nil
 }
 
--- syllogism_test.go
+++ syllogism_test.go
@@ -1,143 +1,30 @@
 package syllogism
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestSyllogism(t *testing.T) {
 	testcases := []struct {
-		syllogism  Syllogism
-		conclusion Conclusion
+		major      Proposition
+		minor      Proposition
+		conclusion Proposition
 	}{
 		{
-			Syllogism{
-				Proposition{
-					Type:      0,
-					Subject:   Term{"rational", "animal"},
-					Verb:      "be",
-					Predicate: Term{"mortal"},
-				},
-				Proposition{
-					Type:      1,
-					Subject:   Term{"socrates"},
-					Verb:      "be",
-					Predicate: Term{"rational", "animal"},
-				},
-				Term{"rational", "animal"},
-			},
-			Conclusion{
-				Term: Term{"socrates", "mortal"},
-				Verb: "be",
-			},
-		},
-		{
-			Syllogism{
-				Proposition{
-					Type:      0,
-					Subject:   Term{"rational", "animal"},
-					Verb:      "be",
-					Negative:  true,
-					Predicate: Term{"mortal"},
-					False:     true,
-				},
-				Proposition{
-					Type:      1,
-					Subject:   Term{"socrates"},
-					Verb:      "be",
-					Predicate: Term{"rational", "animal"},
-				},
-				Term{"rational", "animal"},
-			},
-			Conclusion{
-				Term: Term{"socrates", "mortal"},
-				Verb: "be",
-			},
-		},
-		{
-			Syllogism{
-				Proposition{
-					Type:      0,
-					Subject:   Term{"rational", "animal"},
-					Verb:      "be",
-					Predicate: Term{"mortal"},
-				},
-				Proposition{
-					Type:      1,
-					Subject:   Term{"socrates"},
-					Verb:      "be",
-					Negative:  true,
-					Predicate: Term{"rational", "animal"},
-					False:     true,
-				},
-				Term{"rational", "animal"},
-			},
-			Conclusion{
-				Term: Term{"socrates", "mortal"},
-				Verb: "be",
-			},
-		},
-		{
-			Syllogism{
-				Proposition{
-					Type:      0,
-					Subject:   Term{"rational", "animal"},
-					Verb:      "be",
-					Negative:  true,
-					Predicate: Term{"mortal"},
-					False:     true,
-				},
-				Proposition{
-					Type:      1,
-					Subject:   Term{"socrates"},
-					Verb:      "be",
-					Negative:  true,
-					Predicate: Term{"rational", "animal"},
-					False:     true,
-				},
-				Term{"rational", "animal"},
-			},
-			Conclusion{
-				Term: Term{"socrates", "mortal"},
-				Verb: "be",
-			},
-		},
-		{
-			Syllogism{
-				Proposition{
-					Type:      0,
-					Subject:   Term{"bird"},
-					Verb:      "lay",
-					Predicate: Term{"eggs"},
-				},
-				Proposition{
-					Type:      1,
-					Subject:   Term{"elephant"},
-					Verb:      "be",
-					Negative:  true,
-					Predicate: Term{"bird"},
-				},
-				Term{"bird"},
-			},
-			Conclusion{
-				Term:     Term{"elephant", "eggs"},
-				Verb:     "lay",
-				Negative: true,
-			},
+			Proposition{[]string{"rational animal"}, []string{"mortal"}},
+			Proposition{[]string{"socrates"}, []string{"rational animal"}},
+			Proposition{[]string{"socrates"}, []string{"mortal"}},
 		},
 	}
 	for _, tc := range testcases {
-		conclusion, err := tc.syllogism.Conclude()
+		conclusion, err := Conclude(tc.major, tc.minor)
 		if err != nil {
 			t.Fatal(err)
 		}
-		if !eq(conclusion.Term, tc.conclusion.Term) {
-			t.Fatalf("got conclusion terms: %v\nexpected conclusion terms: %v", conclusion.Term, tc.conclusion.Term)
+		if !reflect.DeepEqual(conclusion, tc.conclusion) {
+			t.Fatalf("\nGot: %+v\nExp: %+v", conclusion, tc.conclusion)
 		}
-		if conclusion.Verb != tc.conclusion.Verb {
-			t.Fatalf("got conclusion verb: %v\nexpected conclusion verb: %v", conclusion.Verb, tc.conclusion.Verb)
-		}
-		if conclusion.Negative != tc.conclusion.Negative {
-			t.Fatalf("got conclusion negative: %v\nexpected conclusion negative: %v", conclusion.Negative, tc.conclusion.Negative)
-		}
+		t.Logf("\nmajor: %v\nminor: %v\nconcl: %v", tc.major, tc.minor, conclusion)
 	}
 }

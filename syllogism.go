3cd1c0272038757717cb6f320a78328692bdc859
Jozef Reisinger <jozef.reisinger@gmail.com>
2022-06-12 15:42:32 +0200

    minor changes in syllogism.go

--- syllogism.go
+++ syllogism.go
@@ -2,10 +2,11 @@
 // syllogism. Syllogism is part of Aristotelian (or common sense) logic.
 //
 // Syllogism connects the subject and predicate terms of its conclusion through
-// its middle term in its two propositions. The first of which states a general
-// principle and the second brings a particular case under that principle. The
-// conclusion then demonstrates the result of applying the general principle to
-// the particular case.
+// its middle term in its two propositions. The first of which (major
+// proposition) states a general principle and the second (minor proposition)
+// brings a particular case under that principle. The conclusion then
+// demonstrates the result of applying the general principle to the particular
+// case.
 package syllogism
 
 import "fmt"

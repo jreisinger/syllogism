a72f9e6d97edb07fd2628584657fb9ad252a4130
Jozef Reisinger <jozef.reisinger@gmail.com>
2022-09-24 18:08:18 +0200

    simplify docs

--- syllogism.go
+++ syllogism.go
@@ -1,18 +1,11 @@
 // Package syllogism implements the basic form of deductive argument, a
 // syllogism. Syllogism is part of Aristotelian (or common sense) logic.
-//
-// Syllogism connects the subject and predicate terms of its conclusion through
-// its middle term in its two propositions. The first of which (major
-// proposition) states a general principle and the second (minor proposition)
-// brings a particular case under that principle. The conclusion then
-// demonstrates the result of applying the general principle to the particular
-// case.
 package syllogism
 
 import "fmt"
 
-// Proposition states something about all or some of the the subjects. It can be
-// affirmative or negative.
+// Proposition in its predicate states something about all or some of the the
+// subjects. It can be affirmative or negative.
 type Proposition struct {
 	Some      bool   // or all
 	Subject   string // term about which something is said in the predicate
